e09f6b58eed3bd782594236f99fdcb13e3b9203a
Valentin <volantinfornes@gmail.com>
2024-10-20 12:53:09 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,31 @@
+import { renderCategories } from "./src/services/categories";
+import { handleSearchProductByName } from "./src/services/searchBar";
+import { openModal } from "./src/views/modal";
+import { handleGetProductsToStore } from "./src/views/store";
+import "./style.css"
+
+/* ===== APLICACION ===== */
+export let categoriaActiva = null;
+export const setCategoriaActiva = (categoriaIn) => {
+    categoriaActiva = categoriaIn;
+};
+
+export let productoActivo = null;
+export const setProductoActivo = (productoIn) => {
+    productoActivo = productoIn;
+};
+
+handleGetProductsToStore();
+renderCategories();
+
+//HEADER
+const buttonAdd = document.getElementById("ButtonAddElement");
+buttonAdd.addEventListener("click", () => {
+    openModal();
+});
+
+const buttonSearch = document.getElementById("ButtonSearch");
+buttonSearch.addEventListener("click", () => {
+    handleSearchProductByName();
+});
+
---
+++ src/services/product.js
@@ -0,0 +1,67 @@
+import Swal from "sweetalert2";
+import { productoActivo } from "../../main";
+import { handleGetProductLocalStorage, setInLocalStorage } from "../persistence/localStorage";
+import { closeModal } from "../views/modal";
+import { handleGetProductsToStore, handleRenderList } from "../views/store";
+
+export const handleSaveOrModifyElements = () => {
+    const nombre = document.getElementById("nombre").value,
+        imagen = document.getElementById("img").value,
+        precio = document.getElementById("precio").value,
+        categories = document.getElementById("categoria").value;
+    let object = null;
+
+    if (productoActivo) {
+        object = {
+            ...productoActivo,
+            nombre,
+            imagen,
+            precio,
+            categories,
+        };
+    }else {
+        object = {
+            id: new Date().toISOString(),
+            nombre,
+            imagen,
+            precio,
+            categories,
+        };
+        console.log(object);
+    };
+
+    Swal.fire({
+        title: "Buen trabajo!",
+        text: "El elemento fue añadido correctamente!",
+        icon: "success"
+    });
+    
+    setInLocalStorage(object);
+    handleGetProductsToStore();
+    closeModal();
+};
+
+export const handleDeleteProduct = () => {
+
+    Swal.fire({
+        title: "Estas seguro?",
+        text: "No podras recuperarlo!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, Eliminalo!"
+        }).then((result) => {
+        if (result.isConfirmed) {
+            const products = handleGetProductLocalStorage();
+            const result = products.filter((el) => el.id !== productoActivo.id);
+            localStorage.setItem("products", JSON.stringify(result));
+            const newProducts = handleGetProductLocalStorage();
+            handleRenderList(newProducts);
+            closeModal();
+        } else {
+            closeModal();
+        }
+    });
+
+};
